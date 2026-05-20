a60640ce69c830121a035b780693fa484b7ed6a0
monicarssi65-source <monicarssi65@gmail.com>
2026-05-20 12:42:17 +0200

    Add app/src/main/java/com/torneicalcio/app/MainActivity.java

---
+++ app/src/main/java/com/torneicalcio/app/MainActivity.java
@@ -0,0 +1,45 @@
+package com.torneicalcio.app;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    private WebView webView;
+
+    @SuppressLint("SetJavaScriptEnabled")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        webView = findViewById(R.id.webView);
+
+        WebSettings settings = webView.getSettings();
+        settings.setJavaScriptEnabled(true);
+        settings.setDomStorageEnabled(true);
+        settings.setAllowFileAccessFromFileURLs(true);
+        settings.setAllowUniversalAccessFromFileURLs(true);
+        settings.setCacheMode(WebSettings.LOAD_NO_CACHE);
+
+        DatabaseHelper dbHelper = new DatabaseHelper(this);
+        DatabaseBridge bridge = new DatabaseBridge(this, dbHelper, webView);
+        webView.addJavascriptInterface(bridge, "AndroidDB");
+
+        webView.setWebViewClient(new WebViewClient());
+        webView.loadUrl("file:///android_asset/index.html");
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (webView.canGoBack()) {
+            webView.goBack();
+        } else {
+            super.onBackPressed();
+        }
+    }
+}
